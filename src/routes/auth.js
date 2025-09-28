58325b32be6b798b247e2facfd4ee8e2185f8a8a
AndriyAtWork <andriytsar3388@gmail.com>
2025-09-28 13:31:24 +0200

    Add validation for auth and posts with error handling

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 // src/routes/auth.js
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authControllers.js";
+import { validateRegister, validateLogin } from "../validators/authValidators.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/register", validateRegister, registerUser);
+router.post("/login", validateLogin, loginUser);
 
 export default router;
